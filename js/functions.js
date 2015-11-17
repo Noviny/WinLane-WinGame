a5375e1c94577a2e17c0863a14f5259a09bdea5c
Noviny BC <benjaminconolly@gmail.com>
2015-11-17 23:25:51 +1100

    js functions for each game part. None currently function

--- js/functions.js
+++ js/functions.js
@@ -7,30 +7,30 @@
 var playerHand = [0, 1, 2, 3, 4, 5];
 var computerHand = [0, 1, 2, 3, 4, 5];
 
+
+//======================
+//Currently bugtesting
+
+
+
+
 var lanes = {
 	1: {
-	player: [5, 3, 2, 0],
-	computer: []
+	player: [5, 3, "hidden", 2, 0],
+	computer: [],
+	staging: 1,
 	},
 	2: {
 	player: [],
-	computer: []
+	computer: [],
+	staging: null,
 	},
 	3: {
 	player: [],
-	computer: []
+	computer: [],
+	staging: null,
 	},
 };
-
-
-var lastCard = {
-	1: ["player", 3],
-	2: null,
-	3: null,
-}
-
-
-
 
 // playerChoice
 var playerChoice = function(choice) {
@@ -40,46 +40,60 @@ var playerChoice = function(choice) {
 }
 
 //We can make this track who the player is later, and vary the splice command
-var takeTurn = function (cardChoice, laneChoice) {
-	if ( playerHand.indexOf(cardChoice) >= 0 ) {
-		// console.log(playerHand.indexOf(cardChoice))
-		var choice = playerHand.splice(playerHand.indexOf(cardChoice), 1);
-		console.log(choice);
-		lanes[laneChoice]["player"].push(Number(choice));
-		lastCard[laneChoice] = ["player", cardChoice];
+var playerTurn = function (cardChoice, laneChoice) {
+	if lanes[laneChoice]["staging"] = null {
+		if ( playerHand.indexOf(cardChoice) >= 0 ) {
+			// console.log(playerHand.indexOf(cardChoice))
+			var choice = playerHand.splice(playerHand.indexOf(cardChoice), 1);
+			console.log(choice);
+			lanes[laneChoice]["player"].push("hidden");
+			lanes[laneChoice]["staging"] = choice;
+
+			// lastCard[laneChoice] = ["player", cardChoice];
+		}
 	} else {
-		return "Invalid choice.";
+		//In here you have to move the items out of the staging area
+		//replicate to replace hidden in either player or computer
+		if ( lanes[lanechoice]["player"].indexOf("hidden") >= 0 ) {
+		lanes[laneChoice]["player"].indexOf("hidden") = lanes[laneChoice]["staging"];
+		lanes[laneChoice]["staging"] = cardChoice
+		} else if ( lanes[lanechoice]["computer"].indexOf("hidden") >= 0 ) {
+			lanes[laneChoice]["player"].indexOf("hidden") = lanes[laneChoice]["staging"];
+		lanes[laneChoice]["staging"] = cardChoice
+		}
+		//then replicate what you have above for the staging being none.
 	}
 }
 
 
-
-
-
-
-
-//Holy shit the hidden information makes this display properties super difficult.
-// It needs to track what the last thing to be put into a lane was. Need a function for that.
-var displayLane = function () {
-//We can add a while clause here to run through all the objects in lastCard
-	if ( lastCard[1] !== null ) {
-		var editedL1 = lanes[1]
-
-
-
-	} else console.log ("blurbl")
+var compTurn = function () {
+	//Pick a random card, play it to the lane where they have the lowest total
+	//If there is a tie, then randomise between the lowest totals.
 }
 
 
 
 
+//======================
+//The below was solved more eloquently.
+//Printing current info no longer needs a function.
+// //Holy shit the hidden information makes this display properties super difficult.
+// // It needs to track what the last thing to be put into a lane was. Need a function for that.
+// var displayLane = function () {
+// //We can add a while clause here to run through all the objects in lastCard
+// 	if ( lastCard[1] !== null ) {
+// 		var editedL1 = lanes[1]
 
 
 
 
-
-
-
+var playGame = function () {
+	while (playerHand+computerhand > 0) {
+		playerTurn();
+		computerturn();
+	}
+	findWinner();
+}
 
 
 var findWinner = function () {
---
+++ readme.md
@@ -0,0 +1,33 @@
+#Win Lane, Win Game
+
+This is a game being built by Ben 'Noviny' Conolly. The game can easily be played with a deck of playing cards, but is being built online as a personal project. Below are the game's rules, as well as some design notes.
+
+Currently the game is designed for two players. The design notes document will detail some of the possible design directions, as well as easy ways to mod the game for more players.
+
+##Rules
+
+###Goal
+The goal in WLWG is to have the highest total in two out of three lanes at the end of a game, once each player has played all their cards.
+
+###Setup
+
+1. Each player is given a hand with cards one to five in it (ace as one for a traditional deck) as well as a wildcard (a jack will do if you have no jokers)
+2. Three lanes are established in the table by placing a jak, a queen and a king in the middle of the table. Alternately, you can split a sheet of paper in to three lanes.
+3. Players take turns playing one card each, choosing which lane to play a card to.
+4. The last card played to a lane by either player is left face-down. Playing a new card to a lane flips up any other face-down card in that lane.
+5. The wildcard is worth no points on its own, but doubles the value of cards you have played to the lane it is in.
+
+###Design Notes/Advanced Notes
+ While at a surface level, the game is about simple sums, the fun and difficulty of the game involves guessing at hidden information, making informed choices, and decieving your opponents about your actions. This is where the fun comes from.
+
+ There are a huge number of variables that could be used to make this game more complicated, however doing so could detract from the core gameplay experience (the whole information guessing), so while I am likely to test variables, I recommend playing the base game, even if it looks simple to you.
+
+ It is likely that player 2 has a slight advantage, as they get to play a card while the opponent has no information. Whether this is small or game-breaking, testing has not yet shown.
+
+###Variant Rules
+ If you believe player 2 is overly advantaged, you can change to a variant where player one plays 2 card on each turn, while player two plays 3 cards their first turn, 2 cards their next turn, and 1 card their final turn. If you play this variant, remember that only one card can be face-down in a lane, even if a player commits multiple cards there on a single one of their turns.
+
+ If you are playing this variant, you may want to allow players to play cards one at a time, to see what is flipped over with each card played before choosing to commit another one.
+
+###Current coding progress
+I have functions for almost every element of the game. Most do not work, but now I can see all the pieces, I can work on making them operate. See functions.js for where I am working.
