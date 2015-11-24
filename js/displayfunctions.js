b0e388c6ae16782696932ba50eab6b11a55979eb
Noviny BC <benjaminconolly@gmail.com>
2015-11-24 15:15:03 +1100

    Player moves all function

--- js/displayfunctions.js
+++ js/displayfunctions.js
@@ -10,19 +10,21 @@ $(document).ready (function () {
 
 
 
-	var cardSelect = function () {
-
+	var unHide = function () {
+		
 	}
+
 	var $selectedCard = null;
 	var cardValue = null;
 
 
-
+//Click on a card to select it: 
 	$(".card").click( function () {
 		//This function is complicated enough
 		//that it should be moved out to its own cardSelect function
 		if ($(this).parent().attr("class") === "playerhand" ) {
-			$selectedCard = this;
+			$selectedCard = $(this);
+			// debugger;
 			cardValue = $(this).find("p").html()
 			//Shows us what card is selected
 			//Does not ungreen if unselected
@@ -30,19 +32,45 @@ $(document).ready (function () {
 		}
 	});
 
+//Click on a lane to assign a selected card to it
+//Big bug, clicking a lane moves a dev,
+//so that bit won't play, bit it still pushes the playerHand
+//Also, all the code here is hardcoded for the player
 	$(".yours").click( function () {
-		if ( $(this).parents().attr("class") === "lane 1" ) {
+		//Stop values being incorrectly lost from playerHand
+		// if (  )
+		//Unhiding goes here
+		//I beleive this will always check the first div?
+
+		if ( $(this).parents().hasClass( "1" ) ) {
+			// if ( $(this).find("p").html() === "hid" ) {
+			// 	console.log($(this).find("hid").html())
+			// 	$(this).find("p").html(lanes[1]["staging"])
+			// $(".hid").removeClass("hid")
+			// }
+			if ( $(this).find('.hid').length ) {
+				$(this).find('.hid').html(lanes[1]["staging"])
+				$(this).find('.hid').removeClass('hid')
+			}
 			myTurn(cardValue, 1)
 		}
-		if ( $(this).parents().attr("class") === "lane 2" ) {
+		if ( $(this).parents().hasClass("2") ) {
+			if ( $(this).find('.hid').length ) {
+				$(this).find('.hid').html(lanes[2]["staging"])
+				$(this).find('.hid').removeClass('hid')
+			}
 			myTurn(cardValue, 2)
 		}
-		if ( $(this).parents().attr("class") === "lane 3" ) {
+		if ( $(this).parents().hasClass("3") ) {
+			if ( $(this).find('.hid').length ) {
+				$(this).find('.hid').html(lanes[3]["staging"])
+				$(this).find('.hid').removeClass('hid')
+			}
 			myTurn(cardValue, 3)
 		}
-		// console.log("registered");
-		// console.log($selectedCard);
 		//The editing of the backend func
+		$selectedCard.find("p").addClass('hid');
+		$selectedCard.find('.hid').html('hid');
 		$(this).append($selectedCard);
 		// playerHand.shift();
 		if ( playerHand.length /*+ computerHand.length*/ <= 0 ) {
@@ -52,6 +80,28 @@ $(document).ready (function () {
 
 
 
+
+
+
 })
 
-//.click should change hand and 
+
+
+
+
+
+
+
+//Code from Jack to study
+
+// var hiddenEl = null;
+
+// $(".yours").eq(0).find("p").each(function (i, el) {
+//   console.log( $(this) );
+//   console.log( i );
+//   console.log( el );
+//   if ( $(this).html() === "hid" ) {
+//     hiddenEl = $(this);
+//     return;
+//   } 
+// });
--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,7 @@
 // Probably used the xHand.slice(xHand.Search())
 // Try and find something more concise
 var playerHand = [1, 2, 3, 4, 5, "tac"];
-//var computerHand = [0, 1, 2, 3, 4, 5];
+var computerHand = [0, 1, 2, 3, 4, 5];
 
 
 
@@ -14,18 +14,21 @@ var lanes = {
 	player: [],
 	computer: [],
 	staging: null,
+	lanesum: 0
 	},
 
 	2: {
 	player: [],
 	computer: [],
 	staging: null,
+	lanesum: 0
 	},
 
 	3: {
 	player: [],
 	computer: [],
 	staging: null,
+	lanesum: 0
 	},
 };
 
@@ -54,14 +57,15 @@ var myTurn = function (cardNum, lane) {
 	playerHand.splice( playerHand.indexOf(parseInt(cardNum)), 1 );
 }
 
-var calcLanes = function () {
+var calcPlayerLanes = function () {
+	// var lanesum[i] = 0
 	for (var i = 1; i <= 3; i++) {
-		var lanesum = 0
 		if ( lanes[i]["player"].indexOf("hid") >= 0 ) {
 			var oldVal = lanes[i]["staging"];
 			var oldHid = lanes[i]["player"].indexOf("hid");
 			lanes[i]["player"][oldHid] = oldVal;
 		}
+		// debugger;
 		//=============================
 		//Where we were working
 		console.log(lanes[i]["player"][i])
@@ -69,12 +73,45 @@ var calcLanes = function () {
 		// if ( lanes[i]["player"].indexOf("tac") >= 0 ) {
 		// 	lanes[i]["player"] = 
 		// }
-		for (var i = 0; i < lanes[i]["player"].length; i++) {
-			var toAdd = lanes[i]["player"][i];
-			lanesum += lanes[i]["player"][i];
+		for (var j = 0; j < lanes[i]["player"].length; j++) {
+			var toAdd = lanes[i]["player"][j];
+			lanes[i]["lanesum"] += parseInt(lanes[i]["player"][j]);
 		};
 	};
+	// debugger;
+	// console.log(lanesum)
 }
+
+//============================
+//Computer random turn
+//============================
+//Because lanes[var]["player"] was hard-coded,
+//we had to rewrite the whole function
+//Before 2P game, we need to combine this into one function
+//It will need to take an extra variable for the player
+var compTurn = function () {
+	var pickLane = Math.round(Math.random()*3);
+	console.log ("picklane is " + pickLane);
+	var pickCard = Math.round(Math.random()*computerHand.length) - 1;
+	console.log("pickCard is " + pickCard);
+
+	if ( lanes[pickLane]["staging"] === null ) {
+		lanes[pickLane]["staging"] = pickCard
+		lanes[pickLane]["computer"].push("hid")
+	} else if ( lanes[pickLane]["computer"].indexOf("hid") >= 0 ) {
+		//replaces hid and stages next num
+		var oldVal = lanes[pickLane]["staging"]
+		//Only copes with computer is hid, not opponent
+		var oldHid = lanes[pickLane]["computer"].indexOf("hid")
+		lanes[pickLane]["computer"][oldHid] = oldVal;
+		lanes[pickLane]["staging"] = pickCard
+		lanes[pickLane]["computer"].push("hid")
+	}
+	computerHand.splice( computerHand.indexOf(parseInt(pickCard)), 1 );
+	return [pickLane, pickCard]
+}
+
+
 
 
 //We can make this track who the player is later, and vary the splice command
@@ -124,32 +161,32 @@ var calcLanes = function () {
 // 		var editedL1 = lanes[1]
 
 
-var findWinner = function () {
-	//Lane1 section
-	var playerTotalLane1 = 0;
-	for (var i = 0; i < lanes[1]["player"].length; i++) {
-		playerTotalLane1 = playerTotalLane1 + lanes[1]["player"][i];
-	};
-	if (lanes[1]["player"].indexOf(0) >= 0) {
-		playerTotalLane1 = playerTotalLane1 * 2;
-	}
-	console.log ( "Player one's total in lane one was " + playerTotalLane1 );
-	var compTotalLane1 = 0
-	for (var i = 0; i < lanes[1]["computer"].length; i++) {
-		compTotalLane1 = compTotalLane1 + lanes[1]["computer"][i];
-	};
-	if (lanes[1]["computer"].indexOf(0) >= 0) {
-		compTotalLane1 = compTotalLane1 * 2;
-	}
-	console.log ( "Computer's total in lane one was " + compTotalLane1 );
-	if ( playerTotalLane1 > compTotalLane1 ) {
-		console.log ("Player wins the lane");
-	} else if ( compTotalLane1 > playerTotalLane1 ) {
-		console.log ("Computer one the lane");
-	} else {
-		("This lane is a draw")
-	}
-}
+// var playerLaneSums = function () {
+// 	//Lane1 section
+// 	var playerTotalLane1 = 0;
+// 	for (var i = 0; i < lanes[1]["player"].length; i++) {
+// 		playerTotalLane1 = playerTotalLane1 + lanes[1]["player"][i];
+// 	};
+// 	if (lanes[1]["player"].indexOf(0) >= 0) {
+// 		playerTotalLane1 = playerTotalLane1 * 2;
+// 	}
+// 	console.log ( "Player one's total in lane one was " + playerTotalLane1 );
+// 	var compTotalLane1 = 0
+// 	for (var i = 0; i < lanes[1]["computer"].length; i++) {
+// 		compTotalLane1 = compTotalLane1 + lanes[1]["computer"][i];
+// 	};
+// 	if (lanes[1]["computer"].indexOf(0) >= 0) {
+// 		compTotalLane1 = compTotalLane1 * 2;
+// 	}
+// 	console.log ( "Computer's total in lane one was " + compTotalLane1 );
+// 	if ( playerTotalLane1 > compTotalLane1 ) {
+// 		console.log ("Player wins the lane");
+// 	} else if ( compTotalLane1 > playerTotalLane1 ) {
+// 		console.log ("Computer one the lane");
+// 	} else {
+// 		("This lane is a draw")
+// 	}
+// }
 
 
 // Can replicate playerChoice
